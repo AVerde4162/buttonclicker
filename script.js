38949ff315dae0fbcdf0b9dc7f1f797737c2aa2d
Averde4162 <Angelloverde4162@gmail.com>
2021-11-18 16:46:43 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function login(element){
+    console.log("this clicked",element);
+}
+function login(element){
+    element.innerText = "sign in"
+}
+function remove(element){
+    element.remove();
+}
